27c60971b2c943f4735c40f8fa34cdca5fc1c5cd
Nan Yupeng <38316241+Nanyupeng@users.noreply.github.com>
2020-04-20 23:46:19 +0800

    搭建UI界面
    1.菜单UI搭建
    2.游戏暂停UI搭建
    3.排行版UI搭建
    4.游戏结束UI搭建

---
+++ Assets/Scripts/Controller/Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    [HideInInspector]
+    public Model model;//获取model 的引用
+    [HideInInspector]
+    public View view;//获取View的引用
+    void Awake()
+    {
+        model = GameObject.FindGameObjectWithTag("Model").GetComponent<Model>();
+        view = GameObject.FindGameObjectWithTag("View").GetComponent<View>();
+
+    }
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
